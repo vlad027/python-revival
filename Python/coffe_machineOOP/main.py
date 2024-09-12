15b9baf083c6ed85c886fe89e7958f50cb5b66e0
Vladislav Plotnikov <vladplotnikov1975@gmail.com>
2024-09-12 11:57:50 -0500

    Finished the OOP version of coffee machine

--- Python/coffe_machineOOP/main.py
+++ Python/coffe_machineOOP/main.py
@@ -5,26 +5,20 @@ from money_machine import MoneyMachine
 money_machine = MoneyMachine()
 menu = Menu()
 coffee_maker = CoffeeMaker()
-menu_item = MenuItem()
-
-options = menu.get_items()
 
 is_on = True
 
-
 while is_on:
+    options = menu.get_items()
     user_choice = input(f"What would you like? ({options})")
+
     if user_choice == "off":
         is_on = False
     elif user_choice == "report":
         coffee_maker.report()
         money_machine.report()
     else:
-        if user_choice in options:
-            sufficient_resources = coffee_maker.is_resource_sufficient(user_choice)
-            if sufficient_resources == True:
-                money_received = money_machine.process_coins()
-                money_machine.make_payment(menu_item.cost)
-                #TODO: finish the rest
-                
-'''OOP version of coffee machine'''
+        drink = menu.find_drink(user_choice)
+        if drink:
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
