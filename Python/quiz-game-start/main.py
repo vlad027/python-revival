8e922619dc4084f72e4b50e17d4858b97d55b22c
Vladislav Plotnikov <vladplotnikov1975@gmail.com>
2024-09-13 22:51:31 -0500

    quiz game OOP

---
+++ Python/quiz-game-start/main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+
+question_bank = []
+
+for questions in question_data:
+    # question_text = questions["text"]
+    # question_answer = questions["answer"]
+    # question_bank[question_text] = question_answer
+
+print(question_bank)
+    
+
